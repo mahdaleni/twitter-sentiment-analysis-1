f0012fa93385f278c5b6483971fb6e90b7207564
Arif <dewisandrax@gmail.com>
2019-09-30 14:44:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from twitter import TwitterClient
 
 app = Flask(__name__)
 # Setup the client <query string, retweets_only bool, with_sentiment bool>
-api = TwitterClient('@yenhsravhsuyip')
+api = TwitterClient('@PrivateCode3')
 
 
 def strtobool(v):
